16b702d5c5c1c372fcb1077d0f14c5c00140dbff
ccactuzz <88033770+ccactuzz@users.noreply.github.com>
2021-08-31 19:21:24 +0300

    Добавлено модальное окно
    Увеличен функционал макета сайта с помощью библиотеки JQuery

--- js/main.js
+++ js/main.js
@@ -3,11 +3,28 @@ $(document).ready(function () {
     var CounterUp = $(".counter-up");
     var CounterDown = $(".counter-down");
     var florPath = $(".home-image path");
+    var modal = $(".modal");
+    var buttonClose = $(".modal-close-button");
+    var flatCheck = $(".button");
+    
+
+  
+
    florPath.on("mouseover",function() {
      currentFlor = $(this).attr('data-flor');
      florPath.removeClass('current-flor')
      $(".counter").text(currentFlor)
    });
+   florPath.on("click",function(){
+      modal.toggleClass("is-open");
+   });
+   buttonClose.on("click",function(){
+     modal.removeClass("is-open");
+   });
+   flatCheck.on("click",function(){
+    modal.toggleClass("is-open");
+ });
+
    CounterUp.on("click",function(){
    if ( currentFlor < 18){
     currentFlor++;
@@ -17,7 +34,6 @@ $(document).ready(function () {
     florPath.removeClass('current-flor')
     $(`[data-flor=${usCurrentFlor}]`).toggleClass("current-flor");
    }
-
    });
    CounterDown.on("click",function(){
     if (  currentFlor > 2 ){
